1eecce709923b3103a674f5166b09600ee24994a
Hamza Aleem <156540037+HamzaAleem326@users.noreply.github.com>
2025-07-30 19:23:53 +0500

    first commit

---
+++ 01-foundation/01_assignment.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel # type:ignore
+
+# todo: create a product model with id,name,price,instock
+
+class Product(BaseModel):
+    id:int
+    name:str
+    price:float
+    instock:bool = True # we can also add default value
+
+GPU = {'id':101, 'name':'Nvidia Rtx 5090', 'price':2000, 'instock':True}
+product = Product(**GPU)
+print(product)
+
+CPU = {'id':102, 'name':'Intel i9 14900K', 'price':600, 'instock':False}
+cpu_product = Product(**CPU)
+print(cpu_product)
